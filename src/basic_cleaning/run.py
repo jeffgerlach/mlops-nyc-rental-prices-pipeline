4af751b733c81bb82e861863d2e8746fe491ef38
Jeff Gerlach <jgerlach@nodecentric.com>
2022-05-06 16:04:19 -0600

    Remove data rows not in proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -39,6 +39,10 @@ def go(args):
     logger.info("Converting last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Remove out of location bounds data
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save data to csv
     logger.info("Generating CSV")
     filename = args.output_artifact
